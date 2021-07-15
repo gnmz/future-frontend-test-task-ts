7e7ccde09a49b0762e5eae743b96e2f3e5f23f54
gnmz <i.popov.developer@gmail.com>
2021-07-15 16:29:23 +0300

    finished ActionModeSelectors

--- src/components/ActionModeSelectors/ActionModeSelectors.tsx
+++ src/components/ActionModeSelectors/ActionModeSelectors.tsx
@@ -1,4 +1,5 @@
-import './ActionModeSelectors.css'
+import { useState } from "react";
+import "./ActionModeSelectors.css";
 
 interface ActionModeSelectorsProps {
   fetchSmallData: () => void;
@@ -6,12 +7,55 @@ interface ActionModeSelectorsProps {
 }
 
 const ActionModeSelectors: React.FC<ActionModeSelectorsProps> = ({
-  fetchSmallData, fetchBigData
+  fetchSmallData,
+  fetchBigData,
 }) => {
+  const [isSmallData, setIsSmall] = useState(false);
+  const [isBigData, setIsBigdata] = useState(false);
+
+  const actions = (id: string) => {
+    switch (id) {
+      case "small":
+        setIsSmall(true);
+        setIsBigdata(false);
+        fetchSmallData();
+        break;
+      case "big":
+        setIsSmall(false);
+        setIsBigdata(true);
+        fetchBigData();
+        break;
+      default:
+        break;
+    }
+  };
+
+  const chooseActionBtn = (id: string) => {
+    if (isSmallData && id === 'small') {
+      return "btn btn-primary";
+    }
+    else if(isBigData && id === 'big'){
+      return "btn btn-primary";
+    }
+    return "btn btn-outline-primary";
+  };
+
   return (
     <div className="action-mode-selectors">
-      <button onClick={fetchSmallData}>Small data</button>
-      <button onClick={fetchBigData}>Big data</button>
+      <button
+        className={chooseActionBtn('small')}
+        onClick={() => actions("small")}
+        style={{ width: "100px" }}
+      >
+        Small data
+      </button>
+      <button
+        className={chooseActionBtn('big')}
+        onClick={() => actions("big")}
+        style={{ width: "100px" }}
+      >
+        Big data
+      </button>
     </div>
   );
 };
